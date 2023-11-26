9420b32974de134d8c8f82fb0e01f77f6b4baf65
Junito Lima <junitolima@MBP-de-Junito.Home>
2023-11-26 15:14:02 +0000

    adding functional support

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import {observer} from 'mobx-react-lite';
+import logo from "./logo.svg";
+import "./App.css";
+import { observer } from "mobx-react-lite";
 
-const App = observer(
-  (props) => {
-    console.log(props.store.count);
+function App(props) {
+  const renderApp = (store) => {
+    console.log(store.count);
     return (
       <div className="App">
-          <h1>Counter</h1>
-          <p>Value: {props.store.count}</p>
-          <button type="button" onClick={() => props.store.increment()}>Increment</button>
-          <button type="button" onClick={() => props.store.decrement()}>Decrement</button>
+        <h1>Counter</h1>
+        <p>Value: {store.count}</p>
+        <button type="button" onClick={() => store.increment()}>
+          Increment
+        </button>
+        <button type="button" onClick={() => store.decrement()}>
+          Decrement
+        </button>
       </div>
     );
-  }
-);
+  };
 
-export default App;
+  return renderApp(props.store);
+}
+
+export default observer(App);
